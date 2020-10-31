72b139dbf8ad57d32a203cb1fbfdf495feeb2025
caroline223 <carolineforrester22@gmail.com>
2020-10-30 21:44:08 -0400

    commit tree de6aaa789994786751e40e3dfd0a1c82bf7377de

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,42 +3,33 @@ def create_an_empty_array
 end
 
 def create_an_array
-  four_elements = ["First element", "Second element", "Third element", "Fourth element" ]
+  ["a", "b", "c", "d"]
 end
 
 def add_element_to_end_of_array(array, element)
-    four_elements = ["First element", "Second element", "Third element", "Fourth element" ]
-    four_elements << "arrays!"
+    array << element
 end
 
 def add_element_to_start_of_array(array, element)
-    four_elements = ["First element", "Second element", "Third element", "Fourth element" ]
-    four_elements.unshift("wow")
+    array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-    four_elements = ["First element", "Second element", "Third element", "Fourth element", "arrays!"]
-    four_elements.pop 
+    array.pop
 end
 
 def remove_element_from_start_of_array(array)
-    four_elements = ["wow", "First element", "Second element", "Third element", "Fourth element", "arrays!"]
-    four_elements.shift
+   array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-    four_elements = ["First element", "Second element", "am", "Fourth element", "arrays!"]
-    four_elements[2]
-  
+    array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-    four_elements = ["wow", "First element", "Second element", "am", "Fourth element", "arrays!"]
-    four_elements.first
-  
+    array.first 
 end
 
 def retrieve_last_element_from_array(array)
-    four_elements = ["wow", "First element", "Second element", "am", "Fourth element", "arrays!"]
-    four_elements.last 
+    array.last 
 end
